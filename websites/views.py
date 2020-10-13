f8af26e4af60a75f5208bbba56f0ba28bb9190eb
Masaki Otsuka <motsuka@soka.edu>
2020-10-13 12:04:35 +0900

    add view and template

--- websites/views.py
+++ websites/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Site
 
 # Create your views here.
+def home(request):
+    sites = Site.objects.all()
+    context = {'sites': sites}
+    return render(request, 'websites/main.html', context)
